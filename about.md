bfb59cd2291ab686b592c4c5944996879e2221a3
OmgRod <89850217+OmgRod@users.noreply.github.com>
2024-12-21 12:29:48 +0000

    initial commit by omgrod yay

--- about.md
+++ about.md
@@ -1,2 +1,3 @@
 # Level Quests
+(unofficial v4 port by OmgRod)
 Adds a basic button to view your quests to the Level menu.
--- src/main.cpp
+++ src/main.cpp
@@ -22,30 +22,6 @@ class $modify(InfoLayerMy, LevelInfoLayer) {
 			auto buttonSpr = CCSprite::create("challengeButton_01.png"_spr);
 			auto buttonSprAlt = CCSprite::create("challengeButton_02.png"_spr);
 			auto sprite = buttonSpr;
-
-			/*if (Mod::get()->getSettingValue<bool>("location")) {					temporarily disabled until proper nodeids for the garage menu releases
-				if(auto menu = this->getChildByID("garage-menu")) {
-					menu->setLayout(
-						RowLayout::create()
-							->setGrowCrossAxis(true)
-							->setAxisReverse(true)
-					);
-					auto button = CCMenuItemSpriteExtra::create(
-						sprite, this, menu_selector(InfoLayerMy::onButton)
-					);
-					sprite->setPositionY(35.250f);
-					button->setSizeMult(1);
-					button->m_animationType = MenuAnimationType::Move;
-					button->m_startPosition = button->getPosition();
-					button->m_offset = ccp(0, -8.f);
-					button->setID("quests-button");
-					menu->setContentSize({ 200.f, 0.f });
-					menu->addChild(button);
-					menu->setPositionX(464.0f);
-					menu->updateLayout();
-				}
-			}
-			else {*/
 				if(auto menu = this->getChildByID("left-side-menu")) {
 					sprite = buttonSprAlt;
 					auto button = CCMenuItemSpriteExtra::create(
@@ -55,7 +31,6 @@ class $modify(InfoLayerMy, LevelInfoLayer) {
 					menu->addChild(button);
 					menu->updateLayout();
 				}
-			//}
 		}
 		return true;
 	} 
